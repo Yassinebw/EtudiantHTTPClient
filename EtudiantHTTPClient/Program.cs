24b82a75533ded8fb36b95929e7b62544cca508a
bnyas <bnyassine216@gmail.com>
2023-12-12 22:42:19 +0100

    Add project files.

---
+++ EtudiantHTTPClient/Program.cs
@@ -0,0 +1,63 @@
+﻿using DAOMysqlLib.Entities;
+using EtudiantHTTPClient.URL;
+using Newtonsoft.Json;
+using System.Text;
+ajouterEtudiant();
+getAllStudent();
+
+
+void getAllStudent()
+{
+    using (var client = new HttpClient())
+    {
+        client.BaseAddress = new Uri("https://localhost:7042/");
+        //HTTP GET
+        var responseTask = client.GetAsync(ApiURL.EtudiantsSearch);//https://localhost:7042/Etudiant
+        responseTask.Wait();
+
+        var result = responseTask.Result;
+        if (result.IsSuccessStatusCode)
+        {
+
+            var readTask = result.Content.ReadAsAsync<Etudiant[]>();
+            readTask.Wait();
+
+            var students = readTask.Result;
+
+            foreach (var student in students)
+            {
+                Console.WriteLine(student.Name);
+            }
+        }
+
+    }
+}
+//ajouterEtudiant();
+static async Task  ajouterEtudiant()
+{
+    using var client = new HttpClient();
+    client.BaseAddress = new Uri("https://localhost:7042/");
+
+    var etudiant = new Etudiant(5, "Sami", 15);
+    
+
+    HttpContent body = new StringContent(JsonConvert.SerializeObject(etudiant), Encoding.UTF8, "application/json");
+    var response = client.PostAsync(ApiURL.AddStudent, body).Result;
+
+    if (response.IsSuccessStatusCode)
+    {
+        var content = await response.Content.ReadAsStringAsync();
+        if (!string.IsNullOrEmpty(content))
+        {
+            var objDeserializeObject = JsonConvert.DeserializeObject<Etudiant>(content);
+
+            Console.WriteLine("Data Saved Successfully.");
+
+            if (objDeserializeObject != null)
+            {
+                Console.WriteLine(objDeserializeObject.Name);
+            }
+        }
+    }
+
+}
---
+++ EtudiantHTTPClient/URL/ApiURL.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EtudiantHTTPClient.URL
+{
+    public static class ApiURL
+    {
+
+       
+        public static string EtudiantsSearch = "/Etudiant";
+        public static string GetSmart = "/Etudiant/Smart";
+        public static string AddStudent = "/Etudiant";
+    }
+
+}
